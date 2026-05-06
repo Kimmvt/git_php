94223900fae1291fd1c77d863482c78dcd12d2c9
“gabrielA” <gabriel.Carvalho2@aluno.ifsp.edu.br>
2026-05-06 11:14:05 -0300

    introdução da frequencia

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
 <?php
-$notas=["nota1"=>readline("Nota 1: "),
+$aluno=["nota1"=>readline("Nota 1: "),
     "nota2"=>readline("Nota 2: "),
     "nota3"=>readline("Nota 3: "),
-    "nota4"=>readline("Nota 4: ")];
+    "nota4"=>readline("Nota 4: "),
+    "freq"=>readLine("Frequencia(100-0): ")];
 $media=0;
-foreach($notas as $chave =>$valor){
-$media=$media+$valor;
-};
-$media=$media/4;
-echo $media;
+$media=($media+$aluno["nota1"]+$aluno["nota2"]+$aluno["nota3"]+$aluno["nota4"])/4;
+echo "Media: $media\n";
+if($aluno["freq"]<75){
+    echo "Reprovado por Falta";
+}else{
+    if($media<6){
+        $notaRec=readline("Nota da recuperação: ");
+        $medFinal=$notaRec+$media;
+        if($medFinal>5){
+            echo"Aprovado na recuperação";
+        }
+        else{
+            echo "Reprovado ate ano que vem";
+        }
+    }
+    else{
+        echo "Aprovado";
+    }
+}
 
 ?>
